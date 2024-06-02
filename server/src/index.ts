ad3df5de57cd29c9065ff06e1b825b5d7bc2193a
adrikum <23457235+adrikum@users.noreply.github.com>
2024-06-02 21:04:20 +0200

    Add ts-node; use correct port

--- server/src/index.ts
+++ server/src/index.ts
@@ -29,6 +29,7 @@ const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEve
   },
 });
 const clientList: ClientList = new ClientList();
+const PORT = process.env.PORT || 3000;
 
 io.on('connection', (socket: Socket) => {
   console.log('Client connected', socket.id);
@@ -57,4 +58,4 @@ io.on('connection', (socket: Socket) => {
   });
 });
 
-httpServer.listen(3000, () => console.log(`Server running on port 3000`));
+httpServer.listen(PORT, () => console.log(`Server running on port ${PORT}`));
