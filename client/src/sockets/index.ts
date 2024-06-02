80c6c056a060b370b34c76f45d64cd68fe42612e
adrikum <23457235+adrikum@users.noreply.github.com>
2024-06-02 22:05:21 +0200

    local server works

--- client/src/sockets/index.ts
+++ client/src/sockets/index.ts
@@ -2,6 +2,7 @@ import { io, Socket } from 'socket.io-client';
 import { ClientToServerEvents, ServerToClientEvents } from '@server/src/index'; // todo gute lsg zum sharen der beiden interfaces?
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(
+  //   'http://localhost:3000',
   'https://battleship-server-4725bfddd6bf.herokuapp.com',
   {
     transports: ['websocket'],
