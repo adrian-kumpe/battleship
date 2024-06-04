065f271db68aaafb36bbdd697b38ee995a931cde
Adrian <23457235+adrikum@users.noreply.github.com>
2024-06-04 14:13:38 +0200

    working alexa connection; add basic room implementation (#2)
    * rm cors option
    
    * add connection_error
    
    * add callback
    
    * add basic room implementation

--- server/src/clients.ts
+++ server/src/clients.ts
@@ -5,23 +5,31 @@ export interface RoomId {
 
 interface Client {
   id: string;
-  name: string;
-  room: RoomId;
+  clientName: string;
+  roomId: RoomId;
 }
 
 export class ClientList {
   private clients: Client[] = [];
 
-  public addClient = (client: Client): { error: string } | undefined => {
+  public checkCreateRoom(client: Client): { error: string } | undefined {
+    const roomAlreadyExisting = this.clients.find((c) => c.roomId.id === client.roomId.id);
+    if (roomAlreadyExisting) return { error: 'Room was already created' };
+  }
+
+  public checkJoinRoom(client: Client): { error: string } | undefined {
     const existingClientName = this.clients.find(
-      (c) => c.name.trim().toLowerCase() === client.name.trim().toLowerCase(),
+      (c) => c.clientName.trim().toLowerCase() === client.clientName.trim().toLowerCase(),
     );
+    const existingSocketId = this.clients.find((c) => c.id === client.id);
     const roomFull =
-      this.clients.filter((c) => c.room.id + c.room.player === client.room.id + client.room.player).length >= 2;
-
-    if (existingClientName) return { error: 'Username has already been taken' };
+      this.clients.filter((c) => c.roomId.id + c.roomId.player === client.roomId.id + client.roomId.player).length >= 2;
+    if (existingClientName) return { error: 'Name has already been taken' };
+    if (existingSocketId) return { error: 'Client already inside a room' };
     if (roomFull) return { error: 'Room is already full' };
+  }
 
+  public addClient = (client: Client) => {
     this.clients.push(client);
   };
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,14 +3,15 @@ import { Server, Socket } from 'socket.io';
 import { ClientList, RoomId } from './clients';
 
 export interface ServerToClientEvents {
-  noArg: () => void;
-  basicEmit: (a: number, b: string, c: Buffer) => void;
-  withAck: (d: string, callback: (e: number) => void) => void;
-  loginError: (error: string) => void;
+  // noArg: () => void;
+  // basicEmit: (a: number, b: string, c: Buffer) => void;
+  // withAck: (d: string, callback: (e: number) => void) => void;
+  notification: (text: string) => void;
 }
 
 export interface ClientToServerEvents {
-  login: (args: { name: string; room: RoomId }) => void;
+  createRoom: (args: { clientName: string; roomId: RoomId }, cb: (error?: string) => void) => void;
+  joinRoom: (args: { clientName: string; roomId: RoomId }, cb: (error?: string) => void) => void;
 }
 
 interface InterServerEvents {
@@ -23,37 +24,61 @@ interface SocketData {
 }
 
 const httpServer = createServer();
-const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
-  cors: {
-    origin: ['http://localhost:3000', 'https://battleship-server-4725bfddd6bf.herokuapp.com'],
-  },
-});
+const io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer);
 const clientList: ClientList = new ClientList();
 const PORT = process.env.PORT || 3000;
 
-io.on('connection', (socket: Socket) => {
-  console.log('Client connected', socket.id);
+io.engine.on('connection_error', (err) => {
+  console.log(err.req); // the request object
+  console.log(err.code); // the error code, for example 1
+  console.log(err.message); // the error message, for example "Session ID unknown"
+  console.log(err.context); // some additional error context
+});
 
-  socket.on('login', (args: { name: string; room: RoomId }) => {
-    console.log('Client joined Room', args.room.id + args.room.player);
-    const { error } =
-      clientList.addClient({
-        id: socket.id,
-        name: args.name,
-        room: args.room,
-      }) ?? {};
-    if (error) {
-      socket.emit('loginError', error);
-      return;
-    }
-    socket.join(args.room.id);
+io.on('connection', (socket: Socket) => {
+  console.info('Client connected', socket.id);
+
+  /** adds client w/ new room to the clientList */
+  // todo spieleinformationen für den raum müssen gespeichert werden
+  socket.on('createRoom', (args: { clientName: string; roomId: RoomId }, cb) => {
+    const client = {
+      id: socket.id,
+      clientName: args.clientName,
+      roomId: args.roomId,
+    };
+    const { error } = clientList.checkCreateRoom(client) ?? clientList.checkJoinRoom(client) ?? {};
+    if (error) return cb(error);
+    console.info(`Room ${args.roomId.id}${args.roomId.player} was created by ${args.clientName} ${socket.id}`);
+    clientList.addClient(client);
+    socket.join(args.roomId.id);
+    io.to(args.roomId.id).emit('notification', `${args.clientName} joined the game`);
+    cb();
   });
 
+  /** adds client w/ existing room to the clientList */
+  socket.on('joinRoom', (args: { clientName: string; roomId: RoomId }, cb) => {
+    const client = {
+      id: socket.id,
+      clientName: args.clientName,
+      roomId: args.roomId,
+    };
+    const { error } = clientList.checkJoinRoom(client) ?? {};
+    if (error) return cb(error);
+    console.info(`Client ${args.clientName} ${socket.id} joined Room ${args.roomId.id}${args.roomId.player}`);
+    clientList.addClient(client);
+    socket.join(args.roomId.id);
+    io.to(args.roomId.id).emit('notification', `${args.clientName} joined the game`);
+    cb();
+  });
+
+  /** removes client from the clientList */
   socket.on('disconnect', () => {
-    console.log('Client disconnected', socket.id);
     const client = clientList.getClient(socket.id);
     if (client) {
+      console.info(`Client ${client.clientName} ${socket.id} disconnected`);
       clientList.deleteClient(socket.id);
+    } else {
+      console.info(`${socket.id} disconnected`);
     }
   });
 });
