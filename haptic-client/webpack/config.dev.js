f470c25116e5abe890d026b626b7b728752b8ca8
adrikum <23457235+adrikum@users.noreply.github.com>
2025-10-24 18:57:46 +0200

    remove phaser stuff; minify

--- haptic-client/webpack/config.dev.js
+++ haptic-client/webpack/config.dev.js
@@ -1,7 +1,6 @@
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
@@ -12,46 +11,19 @@ module.exports = {
     filename: 'bundle.min.js',
   },
   resolve: {
-    extensions: ['.ts', '.js', '.json'],
+    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
         loader: 'ts-loader',
       },
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader',
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml|glsl)$/i,
-        use: 'file-loader',
-      },
     ],
   },
   plugins: [
-    new CleanWebpackPlugin({
-      cleanOnceBeforeBuildPatterns: [path.join(__dirname, 'dist/**/*')],
-    }),
-    new webpack.DefinePlugin({
-      'typeof CANVAS_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_RENDERER': JSON.stringify(true),
-      'typeof WEBGL_DEBUG': JSON.stringify(true),
-      'typeof EXPERIMENTAL': JSON.stringify(true),
-      'typeof PLUGIN_3D': JSON.stringify(false),
-      'typeof PLUGIN_CAMERA3D': JSON.stringify(false),
-      'typeof PLUGIN_FBINSTANT': JSON.stringify(false),
-      'typeof FEATURE_SOUND': JSON.stringify(true),
-    }),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: './index.html',
     }),
